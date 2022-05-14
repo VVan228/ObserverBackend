d25acf95e49705b7bc88e71f06c73ab836dc7874
Leongard <vovchick2210@gmail.com>
2022-05-15 01:35:59 +0800

    Initial commit

---
+++ src/main/java/ru/isu/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package ru.isu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @ResponseBody
+    @RequestMapping("/")
+    public String goHome(){
+        return "home";
+    }
+
+}
