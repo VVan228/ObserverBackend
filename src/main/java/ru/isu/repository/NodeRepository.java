ebf00168eb8494bdbda89dda41e15f274eec345c
Leongard <vovchick2210@gmail.com>
2022-05-16 15:02:16 +0800

    Rebuilt with boot

--- src/main/java/ru/isu/repository/NodeRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.isu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.isu.model.hierarchy.Node;
-
-public interface NodeRepository extends JpaRepository<Node, Long> {
-
-}
